9b6ad264861fb1f0b2e2df20c8891b3409a72938
Ayaj Mulla <ayajmulla2341@gmail.com>
2025-11-30 01:39:22 +0530

    changes

---
+++ app/culture/page.jsx
@@ -0,0 +1,9 @@
+import TraditionsSection from "@/components/traditions-section"
+
+export default function CulturePage() {
+  return (
+    <div className="container mx-auto px-4 py-10">
+      <TraditionsSection />
+    </div>
+  )
+}
