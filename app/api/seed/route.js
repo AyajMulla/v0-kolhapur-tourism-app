3b39fd6499f989eb8cf05721bad10c6fc50169df
Ayaj Mulla <ayajmulla2341@gmail.com>
2026-03-20 00:14:17 +0530

    minor changes

--- app/api/seed/route.js
+++ app/api/seed/route.js
@@ -5,8 +5,6 @@ import Restaurant from '@/lib/models/Restaurant';
 import Hotel from '@/lib/models/Hotel';
 import { touristPlaces, restaurants, hotels } from '@/data/tourism-data';
 
-export const dynamic = 'force-dynamic';
-
 export async function GET() {
   try {
     await connectToDatabase();
